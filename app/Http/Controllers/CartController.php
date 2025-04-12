07f5cd270a6e2c6ab68892ee53f6effa7a27f337
Edin <edin@stree.agency>
2025-04-12 18:10:26 +0200

    cartApi

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -32,15 +32,49 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        if(!auth()->check()){
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        
+        $request->validate([
+            'product_id' => 'required|exists:products,id',
+            'quantity'   => 'required|integer|min:1',
+        ]);
+        
+        $product = \App\Models\Product::findOrFail($request->product_id);
+        
+        $cartItem = Cart::where('user_id', auth()->id())
+            ->where('product_id', $request->product_id)
+            ->first();
+        
+        if ($cartItem) {
+            
+            $cartItem->quantity += $request->quantity;
+            $cartItem->unit_price = $product->price; 
+            $cartItem->save();
+        } else {
+            
+            Cart::create([
+                'user_id'    => auth()->id(),
+                'product_id' => $request->product_id,
+                'quantity'   => $request->quantity,
+                'unit_price' => $product->price,
+            ]);
+        }
+        
+        return response()->json(['message' => 'Produkti u shtua në shportë']);
     }
+
 
     /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
+       
     }
 
     /**
@@ -48,7 +82,23 @@ class CartController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'quantity' => 'required|integer|min:1',
+        ]);
+    
+    
+        $cartItem = Cart::where('user_id', auth()->id())
+            ->where('id', $id)
+            ->firstOrFail();
+    
+        $cartItem->quantity = $request->quantity;
+    
+        
+        $cartItem->unit_price = $cartItem->product->price ?? $cartItem->unit_price;
+    
+        $cartItem->save();
+    
+        return response()->json(['message' => 'Cart updated successfully', 'cartItem' => $cartItem]);
     }
 
     /**
@@ -56,6 +106,12 @@ class CartController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $cartItem = Cart::where('user_id', auth()->id())
+            ->where('id', $id)
+            ->firstOrFail();
+
+        $cartItem->delete();
+
+        return response()->json(['message' => 'Cart item removed successfully']);
     }
 }
