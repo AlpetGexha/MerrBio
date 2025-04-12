dca78b0f04508b33bf7524a59ebf8e1605e86bb9
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-04-13 01:12:53 +0200

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,10 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
-        $middleware->append([
-            StartSession::class,
-            ShareErrorsFromSession::class,
-        ]);
+//        $middleware->append([
+//            StartSession::class,
+//            ShareErrorsFromSession::class,
+//        ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
