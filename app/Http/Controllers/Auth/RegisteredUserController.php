e373e6a70ae23774f62a24152031382225e303e9
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-04-12 23:50:11 +0200

    share

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -28,24 +29,40 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request):JsonResponse
     {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', Rules\Password::defaults()],//'confirmed'
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
         ]);
+
+        if($request->input('is_farmer') === true) {
+            $user->assignRole('farmer');
+        } else {
+            $user->assignRole('customer');
+        }
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return to_route('dashboard');
+//        return to_route('dashboard');
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User registered successfully',
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email,
+                'is_farmer' => $user->hasRole('farmer'),
+            ],
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryList;
 use App\Http\Controllers\CategoryView;
@@ -7,12 +8,13 @@ use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ProductsList;
 use App\Http\Controllers\ProductView;
 use App\Http\Controllers\SendMailController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('users', function (Request $request) {
     return response()->json([
-        'users' => \App\Models\User::all(),
+        'users' => User::all(),
     ]);
 });
 Route::get('user', function (Request $request) {
@@ -25,17 +27,20 @@ Route::get('product/{product_id}', ProductView::class)->name('product.view');
 Route::get('categorys', CategoryList::class)->name('category.view');
 Route::get('category/{category_id}', CategoryView::class)->name('categories.list');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('cart', [CartController::class, 'index']);
-    Route::post('cart/store', [CartController::class, 'store']);
-    Route::put('cart/{id}', [CartController::class, 'update']);
-    Route::delete('cart/{id}', [CartController::class, 'destroy']);
+//Route::middleware('auth:sanctum')->group(function () {
+Route::get('cart', [CartController::class, 'index']);
+Route::post('cart/store', [CartController::class, 'store']);
+Route::put('cart/{id}', [CartController::class, 'update']);
+Route::delete('cart/{id}', [CartController::class, 'destroy']);
 
-    Route::get('locations', [LocationController::class, 'index']);
-    Route::post('locations/store', [LocationController::class, 'store']);
-    Route::get('locations/{id}', [LocationController::class, 'show']);
-    Route::put('locations/{id}', [LocationController::class, 'update']);
-    Route::delete('locations/{id}', [LocationController::class, 'destroy']);
-});
+Route::get('locations', [LocationController::class, 'index']);
+Route::post('locations/store', [LocationController::class, 'store']);
+Route::get('locations/{id}', [LocationController::class, 'show']);
+Route::put('locations/{id}', [LocationController::class, 'update']);
+Route::delete('locations/{id}', [LocationController::class, 'destroy']);
+//});
+
+
+Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
 
 Route::post('sendEmail', SendMailController::class)->name('sendEmail');
