3937e4e7de13ec0b6389234d939281db314e86f7
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-04-20 06:55:24 +0200

    fix fix

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
@@ -33,4 +34,5 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
