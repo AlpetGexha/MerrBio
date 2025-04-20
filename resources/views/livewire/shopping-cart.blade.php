01ff223aa060c28948f6a90cb6386af13c3d36c8
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-04-20 07:14:42 +0200

    x

--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -83,7 +83,7 @@
                         Continue Shopping
                     </a>
                 @else
-                    <a href="{{ route('auth.login') }}"
+                    <a href="{{ route('login') }}"
                         class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-indigo-500 dark:hover:bg-indigo-600">
                         Login to Continue Shopping
                     </a>
